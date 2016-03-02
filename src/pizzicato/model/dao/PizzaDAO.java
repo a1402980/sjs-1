ab41fadd0ccf078d36566f53ec95f276c81dda2d
a1501078 <a1501078@PAO5003Z03.haagahelia.amk>
2016-03-02 15:03:26 +0200

    PizzaDAO ja DataAccessObject

---
+++ src/pizzicato/model/dao/PizzaDAO.java
@@ -0,0 +1,73 @@
+package pizzicato.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import pizzicato.model.Pizza;
+import pizzicato.model.dao.DataAccessObject;
+
+public class PizzaDAO extends DataAccessObject {
+
+		
+		public void addPizza(Pizza pizza) throws SQLException {
+			Connection connection = null;
+			PreparedStatement stmtInsert = null;
+		
+
+			try {
+				connection = getConnection();
+				String sqlInsert = "INSERT INTO pizza(pizza_id, p_nimi, p_hinta, p_saatavuus) VALUES (?, ?, ?, ?)";
+				stmtInsert = connection.prepareStatement(sqlInsert);
+				stmtInsert.setInt(1, pizza.getPizza_id());
+				stmtInsert.setString(2, pizza.getP_nimi());
+				stmtInsert.setDouble(3, pizza.getP_hinta());
+				stmtInsert.setBoolean(4, pizza.isP_saatavuus());
+				stmtInsert.executeUpdate();
+			}catch (SQLException e) {
+				throw new RuntimeException(e);
+			} finally {
+				close(stmtInsert, connection); 
+			}
+			}
+		
+		public ArrayList<Pizza> findAll() {
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			ArrayList<Pizza> pizzat = new ArrayList<Pizza>();
+			Pizza pizza=null;
+			try {
+				conn = getConnection();
+				String sqlSelect ="SELECT pizza_id, p_nimi, p_hinta, p_saatavuus FROM pizza;";
+				stmt=conn.prepareStatement(sqlSelect);
+				rs=stmt.executeQuery(sqlSelect);
+				while(rs.next()) {
+					pizza = readPizza(rs);
+					pizzat.add(pizza);
+				}
+			} catch(SQLException e) {
+				throw new RuntimeException(e);
+			} finally {
+				close(rs,stmt,conn);
+			}
+			
+			return pizzat;
+		}
+		private Pizza readPizza(ResultSet rs) {
+			try {
+				int id=rs.getInt("pizza_id");
+				String nimi=rs.getString("p_nimi");
+				double hinta=rs.getDouble("p_hinta");
+				boolean saatavuus=rs.getBoolean("p_saatavuus");
+				return new Pizza(id, nimi, hinta, saatavuus);
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	
+
+}
