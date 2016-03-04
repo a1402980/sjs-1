8d575b9993c6665064e6d596e0d3cddcd9af0e46
a1500951 <a1500951@PAO5003Z16.haagahelia.amk>
2016-03-04 12:18:06 +0200

    lisää pizzat servlet

--- src/pizzicato/control/ListaaPizzatServlet.java
+++ src/pizzicato/control/ListaaPizzatServlet.java
@@ -1,11 +1,18 @@
 package pizzicato.control;
 
+import RequestDispatcher;
+
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pizzicato.model.Pizza;
+import pizzicato.model.dao.PizzaDAO;
 
 
 @WebServlet("/ListaaPizzatServlet")
@@ -14,11 +21,15 @@ public class ListaaPizzatServlet extends HttpServlet {
        
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PizzaDAO pizzadao = new PizzaDAO();
+		ArrayList<Pizza> pizzat = pizzadao.findAll();
+		
+		request.setAttribute("pizzat", pizzat);
+		
+		String jsp = "/view/pizzalista_omistajalle.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+	
 }
