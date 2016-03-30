cc304a39b87d265952a5e7800dfe7638172b5e4a
a1501078 <a1501078@PAO5003Z02.haagahelia.amk>
2016-03-30 14:54:19 +0300

    Etusivu asiakkaalle

---
+++ src/pizzicato/control/EtusivuServlet.java
@@ -0,0 +1,35 @@
+package pizzicato.control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Etusivu")
+public class EtusivuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public EtusivuServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jsp = "/view/Pizzicato.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
