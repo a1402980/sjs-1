ae2cfb3c7a38f7df23551301c08fded2a469f2fa
a1500951 <a1500951@PAO5003Z19.haagahelia.amk>
2016-03-30 13:29:44 +0300

    Tayte luokka ja lisaatayteservlet muokkausta

--- src/pizzicato/control/LisaaTayteServlet.java
+++ src/pizzicato/control/LisaaTayteServlet.java
@@ -1,6 +1,7 @@
 package pizzicato.control;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pizzicato.model.Pizza;
+import pizzicato.model.Tayte;
+import pizzicato.model.dao.PizzaDAO;
+import pizzicato.model.dao.TayteDAO;
 
 
 @WebServlet("/LisaaTayteServlet")
@@ -24,6 +30,17 @@ public class LisaaTayteServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String tNimi = request.getParameter("nimi");
+		
+		Tayte tayte = new Tayte(tNimi);
+		//*System.out.println(tayte);
+		TayteDAO taytedao = new TayteDAO();
+		try {
+			taytedao.addTayte(tayte);
+		} catch (SQLException e) {
+			System.out.println("Sovelluksessa tapahtui virhe "+ e.getMessage());
+			e.printStackTrace();
+		}
+		response.sendRedirect("...");
 	}
 
 }
