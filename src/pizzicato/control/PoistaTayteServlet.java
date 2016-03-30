b26e20eea2d8d0f606c8748d6a16b133a6e5b1cc
a1500951 <a1500951@PAO5003Z19.haagahelia.amk>
2016-03-30 15:21:04 +0300

    poistatayteservlet

---
+++ src/pizzicato/control/PoistaTayteServlet.java
@@ -0,0 +1,27 @@
+package pizzicato.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pizzicato.model.dao.TayteDAO;
+
+
+@WebServlet("/PoistaTayte")
+public class PoistaTayteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String strId = request.getParameter("tayte_id");
+		int tayteId = new Integer(strId);
+		TayteDAO taytedao = new TayteDAO();
+		taytedao.deleteTayte(tayteId);
+	}
+
+
+}
