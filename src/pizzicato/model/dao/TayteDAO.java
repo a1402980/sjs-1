11f14ecbf79fe38d942761c8bebca93c2897f6f0
a1402980 <a1402980@PAo5003Z18.haagahelia.amk>
2016-03-30 13:39:50 +0300

    tayte

--- src/pizzicato/model/dao/TayteDAO.java
+++ src/pizzicato/model/dao/TayteDAO.java
@@ -12,6 +12,46 @@ import pizzicato.model.Pizza;
 import pizzicato.model.Tayte;
 
 public class TayteDAO extends DataAccessObject{
+	private Pizza readPizza(ResultSet rs) {
+		try {
+			int pizzaId=rs.getInt("tayteid");
+			String pNimi=rs.getString("p_nimi");
+			double pHinta=rs.getDouble("p_hinta");
+			String pSaatavuus=rs.getString("p_saatavuus");				
+			//ArrayList<Tayte> taytteet = new ArrayList<Tayte>();			
+				if (pSaatavuus.equalsIgnoreCase("true")){
+					pSaatavuus = "kyllä";
+				}else if(pSaatavuus.equalsIgnoreCase("false")){
+					pSaatavuus = "ei";
+				}
+			return new Pizza(pizzaId, pNimi, pHinta, pSaatavuus);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+public Tayte findByName(String tNimi) {
+	Connection conn = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+	Tayte tayte=null;
+	try {
+		conn = getConnection();
+		String sqlSelect ="SELECT tayte_id, t_nimi FROM tayte WHERE t_nimi=?;";		
+		stmt=conn.prepareStatement(sqlSelect);
+		stmt.setString(1, tayte.gettNimi());		
+		rs=stmt.executeQuery(sqlSelect);
+		while(rs.next()) {
+			tayte = readPizza(rs);
+		}
+	} catch(SQLException e) {
+		throw new RuntimeException(e);
+	} finally {
+		close(rs,stmt,conn);
+	}
+	
+	return pizza;
+}
 	
 	public void addTayte(Tayte tayte) throws SQLException {
 		Connection connection = null;
@@ -29,6 +69,5 @@ public class TayteDAO extends DataAccessObject{
 			close(stmtInsert, connection); 
 		}
 	}
-	
-	
+
 }
