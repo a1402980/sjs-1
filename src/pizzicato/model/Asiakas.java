a83d9711e03767eff479875bc35ab5ec831daa89
a1500951 <a1500951@PAO5003Z03.haagahelia.amk>
2016-04-20 15:28:33 +0300

    Asiakas, Tilaus ja PizzaTilaus luokkia muokattu

--- src/pizzicato/model/Asiakas.java
+++ src/pizzicato/model/Asiakas.java
@@ -1,13 +1,96 @@
 package pizzicato.model;
 
 public class Asiakas {
-	int asiakasId;
-	int postiNro;
-	String etuNimi; 
-	String sukuNimi;
-	String osoite;
-	String sPosti; 
-	String postiTmp;
+	private int asiakasId;
+	private int postiNro;
+	private String etuNimi; 
+	private String sukuNimi;
+	private String osoite;
+	private String sPosti; 
+	private String postiTmp;
+	
+	public Asiakas() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Asiakas(int asiakasId, int postiNro, String etuNimi,
+			String sukuNimi, String osoite, String sPosti, String postiTmp) {
+		super();
+		this.asiakasId = asiakasId;
+		this.postiNro = postiNro;
+		this.etuNimi = etuNimi;
+		this.sukuNimi = sukuNimi;
+		this.osoite = osoite;
+		this.sPosti = sPosti;
+		this.postiTmp = postiTmp;
+	}
+
+	public int getAsiakasId() {
+		return asiakasId;
+	}
+
+	public void setAsiakasId(int asiakasId) {
+		this.asiakasId = asiakasId;
+	}
+
+	public int getPostiNro() {
+		return postiNro;
+	}
+
+	public void setPostiNro(int postiNro) {
+		this.postiNro = postiNro;
+	}
+
+	public String getEtuNimi() {
+		return etuNimi;
+	}
+
+	public void setEtuNimi(String etuNimi) {
+		this.etuNimi = etuNimi;
+	}
+
+	public String getSukuNimi() {
+		return sukuNimi;
+	}
+
+	public void setSukuNimi(String sukuNimi) {
+		this.sukuNimi = sukuNimi;
+	}
+
+	public String getOsoite() {
+		return osoite;
+	}
+
+	public void setOsoite(String osoite) {
+		this.osoite = osoite;
+	}
+
+	public String getsPosti() {
+		return sPosti;
+	}
+
+	public void setsPosti(String sPosti) {
+		this.sPosti = sPosti;
+	}
+
+	public String getPostiTmp() {
+		return postiTmp;
+	}
+
+	public void setPostiTmp(String postiTmp) {
+		this.postiTmp = postiTmp;
+	}
+
+	@Override
+	public String toString() {
+		return "Asiakas [asiakasId=" + asiakasId + ", postiNro=" + postiNro
+				+ ", etuNimi=" + etuNimi + ", sukuNimi=" + sukuNimi
+				+ ", osoite=" + osoite + ", sPosti=" + sPosti + ", postiTmp="
+				+ postiTmp + "]";
+	}
+	
+	
 	
 
 }
--- src/pizzicato/model/Tilaus.java
+++ src/pizzicato/model/Tilaus.java
@@ -1,11 +1,77 @@
 package pizzicato.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class Tilaus {
-	int tilausId;
-	int asiakasId;
-	String status;
-	Date tilausAjankohta = new Date();
+	private int tilausId;
+	private int asiakasId;
+	private String status;
+	private Date tilausAjankohta = new Date();
+	private ArrayList<PizzaTilaus> pizzatilaukset = new ArrayList<PizzaTilaus>();
+	
+	public Tilaus() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tilaus(int tilausId, int asiakasId, String status,
+			Date tilausAjankohta, ArrayList<PizzaTilaus> pizzatilaukset) {
+		super();
+		this.tilausId = tilausId;
+		this.asiakasId = asiakasId;
+		this.status = status;
+		this.tilausAjankohta = tilausAjankohta;
+		this.pizzatilaukset = pizzatilaukset;
+	}
+
+	public int getTilausId() {
+		return tilausId;
+	}
+
+	public void setTilausId(int tilausId) {
+		this.tilausId = tilausId;
+	}
+
+	public int getAsiakasId() {
+		return asiakasId;
+	}
+
+	public void setAsiakasId(int asiakasId) {
+		this.asiakasId = asiakasId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getTilausAjankohta() {
+		return tilausAjankohta;
+	}
+
+	public void setTilausAjankohta(Date tilausAjankohta) {
+		this.tilausAjankohta = tilausAjankohta;
+	}
+
+	public ArrayList<PizzaTilaus> getPizzatilaukset() {
+		return pizzatilaukset;
+	}
+
+	public void setPizzatilaukset(ArrayList<PizzaTilaus> pizzatilaukset) {
+		this.pizzatilaukset = pizzatilaukset;
+	}
+
+	@Override
+	public String toString() {
+		return "Tilaus [tilausId=" + tilausId + ", asiakasId=" + asiakasId
+				+ ", status=" + status + ", tilausAjankohta=" + tilausAjankohta
+				+ ", pizzatilaukset=" + pizzatilaukset + "]";
+	}
+	
+	
 }
