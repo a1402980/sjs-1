7c263f79f02cf3fb53305084455bfbb64778ca78
a1501078 <a1501078@PAO5003Z02.haagahelia.amk>
2016-04-22 15:37:02 +0300

    ostoskori

--- src/pizzicato/control/OstoskoriServlet.java
+++ src/pizzicato/control/OstoskoriServlet.java
@@ -1,6 +1,8 @@
 package pizzicato.control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,26 +11,52 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import pizzicato.model.Tilaus; 
+import pizzicato.model.PizzaTilaus; 
+import pizzicato.model.dao.TilausDAO; 
+import pizzicato.model.dao.PizzaTilausDAO; 
 
 
-@WebServlet("/Ostoskori")
+
+@WebServlet("/ostoskori")
 public class OstoskoriServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jsp = "/view/pizzicato_ostoskori.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String jsp = "/view/pizzicato_ostoskori.jsp";
 		
-		/*Tilaus tilaus;
+		TilausDAO tilausdao = new TilausDAO();
+		PizzaTilausDAO pizzatilausdao = new PizzaTilausDAO();
+		Tilaus tilaus;
+		PizzaTilaus pizzatilaus;
+		
 		//avataan uusi sessio
 		HttpSession session = request.getSession(true);
+		tilaus = (Tilaus) session.getAttribute("tilaus");
 		
-		if(tilaus !=null) {
+		//jos tilausta ei ole, luodaan uusi tilaus
+		if(tilaus ==null) {
 			tilaus=new Tilaus();
-		}
+			session.setAttribute("tilaus", tilaus);
+		} /*else {
+			tilaus.addPizzaTilaus(pizzatilaus);
+			try {
+				pizzatilausdao.addPizzaTilaus(pizzatilaus);
+			} catch (Exception e) {
+				System.out.println("Sovelluksessa tapahtui virhe "+ e.getMessage());
+				e.printStackTrace();
+			}
+			
+			
+		}*/
 		
-		*/
+		
 		
 	}
 
