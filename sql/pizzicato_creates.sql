8e6fc985cc71ae10a44786f6decab8f3efe2efd9
a1500890 <a1500890@PAO5003Z19.haagahelia.amk>
2016-05-04 13:25:01 +0300

    tilaus_id pizzatilauksesta poistettu

--- sql/pizzicato_creates.sql
+++ sql/pizzicato_creates.sql
@@ -66,11 +66,9 @@ FOREIGN KEY (asiakas_id) REFERENCES asiakas(asiakas_id)
 
 
 CREATE TABLE pizzatilaus(
-tilaus_id int NOT NULL,
 pizza_id int NOT NULL,
 lkm int NOT NULL,
-PRIMARY KEY (tilaus_id, pizza_id),
-FOREIGN KEY (tilaus_id) REFERENCES tilaus(tilaus_id),
+PRIMARY KEY (pizza_id),
 FOREIGN KEY (pizza_id) REFERENCES pizza(pizza_id)
 )Engine="InnoDB";
 
