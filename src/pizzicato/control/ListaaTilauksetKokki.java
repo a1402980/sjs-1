3fbf8770c69a78693247e77ebe9d2d824f928aa8
a1501078 <a1501078@PAO5003Z02.haagahelia.amk>
2016-05-04 16:09:46 +0300

    kokinsivu.jsp liitetty ListaaTilauksetKokki servletiin

--- src/pizzicato/control/ListaaTilauksetKokki.java
+++ src/pizzicato/control/ListaaTilauksetKokki.java
@@ -1,6 +1,8 @@
 package pizzicato.control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,17 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ListaaTilauksetKokki extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-    public ListaaTilauksetKokki() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String jsp = "/view/kokinsivu.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
