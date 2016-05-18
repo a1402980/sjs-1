fc8951edd7aa68e6ab70c6f4ed8a892f78dfc7d8
a1500847 <a1500847@PAO5003Z17.haagahelia.amk>
2016-05-18 16:18:46 +0300

    Tilaus poistuu kunnolla sessiosta kun tilaus on tehty

--- src/pizzicato/control/TilausvahvistusServlet.java
+++ src/pizzicato/control/TilausvahvistusServlet.java
@@ -52,6 +52,7 @@ public class TilausvahvistusServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true);
 		session.removeAttribute("tilaus");
+		response.sendRedirect("Etusivu#pizzamenu"); 
 	}
 
 }
